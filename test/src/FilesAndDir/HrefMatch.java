4cacd844e94a3b4437fe96989d56652cf2b2bbd5
peng <renpeng@orientsec.com.cn>
2021-03-22 17:24:46 +0800

    增加了部分文件io操作

--- test/src/FilesAndDir/HrefMatch.java
+++ test/src/FilesAndDir/HrefMatch.java
@@ -5,6 +5,9 @@ import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.regex.MatchResult;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
 /**
  * Create by peng on 2021/3/20.
@@ -21,12 +24,14 @@ public class HrefMatch {
             String input = new String(in.readAllBytes(), StandardCharsets.UTF_8);
 
             // search for all occurrences of pattern
-
-
-        } catch (IOException e) {
+            String patternString = "<a\\s+href\\s*=\\s*(\"[^\"]*\"|[^\\s>]*)\\s*>";
+            Pattern pattern = Pattern.compile(patternString, Pattern.CASE_INSENSITIVE);
+            pattern.matcher(input)
+                    .results()
+                    .map(MatchResult::group)
+                    .forEach(System.out::println);
+        } catch (IOException | PatternSyntaxException e) {
             e.printStackTrace();
         }
-
-
     }
 }
---
+++ test/src/textFile/TextFileTest.java
@@ -0,0 +1,7 @@
+package textFile;
+
+/**
+ * Create by peng on 2021/3/22.
+ */
+public class TextFileTest {
+}
